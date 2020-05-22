a0c910d9d34660b72cb6033d9831f1c956d83600
Will Ediger <will.ediger@gmail.com>
2020-05-21 19:22:18 -0500

    DLL implementation of stack

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,5 @@
+from doubly_linked_list import DoublyLinkedList
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -10,19 +12,43 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+########################
+# ARRAY IMPLEMENTATION #
+########################
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.append(value)
+
+#     def pop(self):
+#         if len(self):
+#             return self.storage.pop()
+#         else:
+#             return None
+
+
+######################
+# DLL IMPLEMENTATION #
+######################
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = []
-
+        self.storage = DoublyLinkedList()
+    
     def __len__(self):
         return len(self.storage)
 
     def push(self, value):
-        self.storage.append(value)
+        self.storage.add_to_head(value)
 
     def pop(self):
         if len(self):
-            return self.storage.pop()
+            return self.storage.remove_from_head()
         else:
             return None
